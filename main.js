b1e73b97fd15af3e1c2c1864bcdf6063570fbeb8
Krzystof Staroń <kisiel3141@gmail.com>
2021-12-09 11:23:35 +0100

    clear project

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
+const randomInfo = () => {
+  const infoArray = [
+    "test",
+    "test2"
+  ];
+
+  document.getElementById('aInSpan').href = 'info/' + infoArray[(Math.floor(Math.random() * infoArray.length))];
+}
+
 const setup = () => {
   console.log("Setuping");
+  randomInfo();
 }
 
 window.onload = setup;
